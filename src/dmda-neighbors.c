a01124a76c1205729aaa59ec682e1b891fc86b9a
Matthew Young <myoung.space.science@gmail.com>
2024-02-23 10:22:44 -0500

    Create dmda-neighbors.c

---
+++ src/dmda-neighbors.c
@@ -0,0 +1,50 @@
+static char help[] = \
+"Example using DMDAGetNeighbors.";
+
+#include <petsc.h>
+#include <petscdm.h>
+#include <petscdmda.h>
+
+int main(int argc, char **args)
+{
+  DM dmda;
+  PetscMPIInt        rank;
+  PetscInt           nx=32;
+  PetscInt           ny=32;
+  PetscInt           nz=32;
+  DMBoundaryType     xBC=DM_BOUNDARY_PERIODIC;
+  DMBoundaryType     yBC=DM_BOUNDARY_PERIODIC;
+  DMBoundaryType     zBC=DM_BOUNDARY_PERIODIC;
+  PetscInt           dof=1;
+  PetscInt           width=1;
+  PetscReal          x0=0.0;
+  PetscReal          x1=1.0;
+  PetscReal          y0=0.0;
+  PetscReal          y1=1.0;
+  PetscReal          z0=0.0;
+  PetscReal          z1=1.0;
+  const PetscMPIInt *ranks;
+
+  PetscFunctionBeginUser;
+
+  PetscCall(PetscInitialize(&argc, &args, NULL, help));
+
+  PetscCallMPI(MPI_Comm_rank(PETSC_COMM_WORLD, &rank));
+  PetscCall(DMDACreate3d(PETSC_COMM_WORLD, xBC, yBC, zBC, DMDA_STENCIL_BOX, nx, ny, nz, PETSC_DECIDE, PETSC_DECIDE, PETSC_DECIDE, dof, width, NULL, NULL, NULL, &dmda));
+  PetscCall(DMDASetElementType(dmda, DMDA_ELEMENT_Q1));
+  PetscCall(DMSetFromOptions(dmda));
+  PetscCall(DMSetUp(dmda));
+  PetscCall(DMDASetUniformCoordinates(dmda, x0, x1, y0, y1, z0, z1));
+  PetscCall(DMView(dmda, PETSC_VIEWER_STDOUT_WORLD));
+  PetscCall(DMDAGetNeighbors(dmda, &ranks));
+  PetscCall(PetscSynchronizedPrintf(PETSC_COMM_WORLD, "[%d]\n", rank));
+  for (int i=0; i<27; i++) {
+    PetscCall(PetscSynchronizedPrintf(PETSC_COMM_WORLD, "ranks[%02d] = %02d\n", i, ranks[i]));
+  }
+  PetscCall(PetscSynchronizedPrintf(PETSC_COMM_WORLD, "\n"));
+  PetscCall(PetscSynchronizedFlush(PETSC_COMM_WORLD, PETSC_STDOUT));
+
+  PetscCall(PetscFinalize());
+
+  return 0;
+}
